15b320b2d76c2efa3f4fc702dc65532475ed70f6
dofenghost <dofenspot@gmail.com>
2025-03-13 01:04:08 +0700

    Добавил некоторые функции из 5 пункта.

--- backend/routers/car_parser.py
+++ backend/routers/car_parser.py
@@ -15,7 +15,8 @@ def create_db_car(car_in: CarIn) -> Car:
     db_car = Car(**car_in.model_dump())
     return db_car
 
-@router.post("/get_car_info")
+
+@router.post("/get_car_info", tags=["Пункт 4"]) #3001
 async def get_car_info(vin: str = Body(embed=True), db: Session = Depends(get_db)):
     '''
     https://www.17vin.com/doc.html. Раздел 3001.\n
@@ -117,7 +118,7 @@ async def get_car_info(vin: str = Body(embed=True), db: Session = Depends(get_db
 
     return part_data
 
-@router.post("/get_parts_info")
+@router.post("/get_parts_info", tags=["Пункт 4"]) #4002
 async def get_parts_info(epc: str = Body(embed=True), query_part_number: str = Body(embed=True)):
     '''
     https://www.17vin.com/doc.html. Раздел 4002.\n
@@ -141,7 +142,7 @@ async def get_parts_info(epc: str = Body(embed=True), query_part_number: str = B
 
     return part_data
 
-@router.post("/get_part_by_qpn")
+@router.post("/get_part_by_qpn", tags=["Пункт 4"]) #4001
 async def get_part_by_qpn(query_part_number: str = Body(embed=True), query_match_type: str = Body(embed=True, default="smart")):
     '''
     https://www.17vin.com/doc.html. Раздел 4001.\n
@@ -166,7 +167,7 @@ async def get_part_by_qpn(query_part_number: str = Body(embed=True), query_match
 
     return part_data
 
-@router.post("/get_interchange")
+@router.post("/get_interchange", tags=["Пункт 4"]) #4004
 async def get_interchange_by_pn_and_group_id(part_number: str = Body(embed=True), group_id: str = Body(embed=True)):
     '''
     Раздел 4004.\n
@@ -187,8 +188,8 @@ async def get_interchange_by_pn_and_group_id(part_number: str = Body(embed=True)
 
     return part_data
 
-@router.post("/get_accessories_list")
-async def get_accessories_list_by_catalog_code(epc: str = Body(embed=True), cata_code: str = Body(embed=True)):
+@router.post("/get_accessories_list_from_catalogue", tags=["Пункт 4"]) # 4005
+async def get_accessories_list_by_catalogue_code(epc: str = Body(embed=True), cata_code: str = Body(embed=True)):
     '''
     Раздел 4005.\n
     '''
@@ -207,7 +208,7 @@ async def get_accessories_list_by_catalog_code(epc: str = Body(embed=True), cata
 
     return part_data
 
-@router.post("/get_parts_info")
+@router.post("/get_parts_info", tags=["Пункт 4"]) # 40071
 async def get_parts_info_by_brand_and_qpn(brand: str = Body(embed=True), 
                                           query_part_number: str = Body(embed=True), 
                                           query_match_type: str = Body(embed=True, default="smart")):
@@ -231,3 +232,127 @@ async def get_parts_info_by_brand_and_qpn(brand: str = Body(embed=True),
     return part_data
 
 # не реализовал 40031, 40032, 4006, 40073
+
+@router.post("/get_all_part_numbers", tags=["Пункт 5"]) # 5109
+async def get_all_part_numbers_from_vin(epc: str = Body(embed=True), 
+                                        vin: str = Body(embed=True)):
+    '''
+    Раздел 5109.
+    '''
+    url_parameters = f"/{epc}?action=all_part_number&vin={vin}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    numbers = part_data["data"].split("@")
+
+    return {"numbers": numbers}
+
+@router.post("/get_catalogue_1", tags=["Пункт 5", "Каталоги"]) # 5101
+async def get_catalogue_level_1(epc: str = Body(embed=True),
+                                vin: str = Body(embed=True)):
+    url_parameters = f"/{epc}?action=cata1&vin={vin}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    return part_data
+
+@router.post("/get_catalogue_2", tags=["Пункт 5", "Каталоги"]) # 5102
+async def get_catalogue_level_2(epc: str = Body(embed=True), 
+                                vin: str = Body(embed=True), 
+                                cata1_code: str = Body(embed=True)):
+    
+    url_parameters = f"/{epc}?action=cata2&vin={vin}&cata1_code={cata1_code}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    return part_data
+
+@router.post("/get_catalogue_3", tags=["Пункт 5", "Каталоги"]) # 5103
+async def get_catalogue_level_3(epc: str = Body(embed=True), 
+                                vin: str = Body(embed=True), 
+                                cata2_code: str = Body(embed=True)):
+    
+    url_parameters = f"/{epc}?action=cata3&vin={vin}&cata2_code={cata2_code}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    return part_data
+
+@router.post("/get_catalogue_4", tags=["Пункт 5", "Каталоги"]) # 5104
+async def get_catalogue_level_4(epc: str = Body(embed=True), 
+                                vin: str = Body(embed=True), 
+                                cata3_code: str = Body(embed=True)):
+    
+    url_parameters = f"/{epc}?action=cata4&vin={vin}&cata3_code={cata3_code}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    return part_data
+
+@router.post("/get_accessories_list")
+async def get_accessories_list(epc: str = Body(embed=True), 
+                               vin: str = Body(embed=True), 
+                               last_cata_code: str = Body(embed=True),
+                               last_cata_code_level: str = Body()):
+    
+    url_parameters = f"/{epc}?action=part&vin={vin}&last_cata_code={last_cata_code}&last_cata_code_level={last_cata_code_level}"
+    token = generate_token(username=USER, password=PW, url_parameters=url_parameters)
+
+    url = f"{BASE_URL}{url_parameters}&user={USER}&token={token}"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Ошибка при запросе данных")
+    
+    # Преобразуем ответ в JSON
+    part_data = response.json()
+
+    return part_data
+
+
