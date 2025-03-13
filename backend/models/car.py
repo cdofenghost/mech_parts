1562f681946ced4ce589f55ce035f16020615b1d
quilu01 <nikita20055002@mail.ru>
2025-03-13 11:24:32 +0700

    удалил partsSearchHistory из car

--- backend/models/car.py
+++ backend/models/car.py
@@ -29,14 +29,3 @@ class Car(Base):
     body_type = Column(String)
     price = Column(String)
     price_unit = Column(String)
-
-class PartSearchHistory(Base):
-    __tablename__ = "part_search_history"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)  # ID пользователя, сделавшего запрос
-    part_name = Column(String, nullable=False)  # Название запчасти
-    search_count = Column(Integer, default=1)  # Количество поисков этой запчасти
-
-    def __repr__(self):
-        return f"<PartSearchHistory(user_id={self.user_id}, part_name={self.part_name}, search_count={self.search_count})>"
